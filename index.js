416c7496ba3058f1b2e9359f9161c0d0995b68d6
JulianPollos <kreikejulian@gmail.com>
2025-09-18 10:10:00 +0200

    Security verbeterd met hashing en salting via bcrypt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const bcrypt = require('bcrypt'); 
 
 const app = express();
 const port = 3000;
@@ -23,7 +24,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-
 app.get('/game', (req, res) => {
     db.query('SELECT * FROM videogames ORDER BY RAND() LIMIT 1;', (err, results) => {
         if (err) {
@@ -45,15 +45,22 @@ app.get('/game/:id', (req, res) => {
     });
 });
 
-app.post('/postgame/:naam', (req, res) => {
-    const naam = req.params.naam;
-    db.query('INSERT INTO videogames (naam) VALUES (?);', naam, (err, results) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json(results);
-    });
+app.post('/postgame/:naam', async (req, res) => {
+    try {
+        const naam = req.params.naam;
+        
+        const hashedNaam = await bcrypt.hash(naam, 10);
+
+        db.query('INSERT INTO videogames (naam) VALUES (?);', [hashedNaam], (err, results) => {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }
+            res.json({ message: "Game stored with hashed naam!", results });
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
 });
 
 app.delete('/deletegame/:id', (req, res) => {
@@ -66,7 +73,6 @@ app.delete('/deletegame/:id', (req, res) => {
         res.json(results);
     });
 });
-
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
